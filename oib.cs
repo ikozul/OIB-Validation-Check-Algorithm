48fbad3263b6fa07142761f4b77a3e292c96f527
ikozul <ikozul@users.noreply.github.com>
2018-06-28 19:31:05 +0200

    Add files via upload

---
+++ oib.cs
@@ -0,0 +1,27 @@
+﻿
+    public class Oib
+    {
+        internal bool CheckOIB(string oib)
+        {
+            if (oib.Length != 11) return false;
+
+            long b;
+            if (!long.TryParse(oib, out b)) return false;
+
+            int a = 10;
+            for (int i = 0; i < 10; i++)
+            {
+                a = a + Convert.ToInt32(oib.Substring(i, 1));
+                a = a % 10;
+                if (a == 0) a = 10;
+                a *= 2;
+                a = a % 11;
+            }
+            int control = 11 - a;
+            if (control == 10) control = 0;
+
+            return control == Convert.ToInt32(oib.Substring(10, 1));
+        }
+
+
+    }
